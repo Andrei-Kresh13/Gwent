d9c010a2425fc100fbc2fcf1845d58be573dde44
Andrei-Kresh13 <andrei-creshun@mail.ru>
2021-05-25 21:17:43 +0300

    changed

--- app/src/main/java/com/example/gwent/cards/game/GameSession.java
+++ app/src/main/java/com/example/gwent/cards/game/GameSession.java
@@ -8,6 +8,7 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.LinkedList;
 import java.util.List;
+import java.util.Map;
 import java.util.Queue;
 import java.util.Stack;
 
