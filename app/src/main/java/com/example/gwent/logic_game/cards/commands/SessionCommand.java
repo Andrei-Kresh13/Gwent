d41df71dcf6bc276b11dfaafc3a1e0e457744a94
Andrei-Kresh13 <andrei-creshun@mail.ru>
2021-05-28 21:46:04 +0300

    changed

--- app/src/main/java/com/example/gwent/logic_game/cards/commands/SessionCommand.java
+++
@@ -1,9 +0,0 @@
-package com.example.gwent.logic_game.cards.commands;
-
-import com.example.gwent.logic_game.cards.commands.cards.game.GameSession;
-//Defines the command that will be executed on game session
-public interface SessionCommand {
-
-    void setGameSession(GameSession gameSession);
-    void execute();
-}
